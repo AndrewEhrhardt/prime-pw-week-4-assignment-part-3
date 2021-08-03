494ee52be7f9bc4bb56130730addb94d9a055a8e
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-08-02 19:06:54 -0500

    completed stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,15 +3,19 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
+const maxItems = 5;
 
 function addItem(newItem) {
-  basket.push(newItem);
-  return true;
+  if(isFull()){
+    return false;
+  } else {
+    basket.push(newItem);
+    return true;
+  }
 }
 
-let i = 0;
-
 function listItems(){
+  let i = 0;
   while (i < basket.length){
     console.log(basket[i]);
     i++;
@@ -38,17 +42,33 @@ function empty(){
 empty()
 console.log(basket);
 
-console.log("Adding pants to the basket, should return true:", addItem("Pants"));
-console.log("Adding shirt to the basket, should return true:", addItem("Shirt"));
-console.log("Adding shoes to the basket, should return true:", addItem("Shoes"));
-console.log("Adding pants to the basket, should return true:", addItem("Socks"));
-console.log("Adding shirt to the basket, should return true:", addItem("Hat"));
+addItem("Pants");
+addItem("Shirt");
+addItem("Shoes");
+addItem("Socks");
 
-
-const maxItems = 5;
+console.log(basket.length)
 
 function isFull() {
-  return (basket.length <= maxItems ? true : false);
+  return (basket.length >= maxItems);
 }
+//I'd orignally completed the above function using an if else statement, however just using
+//a comparision seems to work fine, is this a better/more efficent way of solving the problem?
 
 console.log(isFull());
+console.log(addItem("Hat"));
+
+listItems();
+
+function removeItem(item){
+  if (basket.indexOf(item) >= 0){
+    basket.splice(basket.indexOf(item), 1)
+    return item;
+  } else {
+    return "null";
+  }
+}
+
+console.log(removeItem("Hat"));
+listItems();
+console.log(removeItem("Scarf"))
